1d3f5e3877d8b028e17164bd514e1216e3358360
fuutou89 <shengdongtan@gmail.com>
2014-09-26 20:52:11 +0800

    EventManager

---
+++ Assets/Scripts/Managers/EventManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using PureMVC.Interfaces;
+
+namespace Core.Manager
+{
+	//C# Unity event manager that uses string in a hashtabel over delegates and events in order to 
+	//allow use of events without knowing where and when they're declared/defined
+
+	public delegate void EventListener(params object[] args);
+
+
+}
