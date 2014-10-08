da30c70f86932649eae7a93c467738f04056b3c8
Fuutou Tan <shengdongtan@gmail.com>
2014-10-08 21:44:03 +0800

    EventManager Update

--- Assets/Scripts/Core/Engine.cs
+++ Assets/Scripts/Core/Engine.cs
@@ -1,11 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using Core.Interface;
 
 /// <summary>
 /// Everything start from here
 /// </summary>
 public class Engine : MonoBehaviour 
 {
+	private static Engine _instance;
+	public static Engine instance { get { return _instance; }}
+
+	#region TickObjectQueue
+	private List<ITickObject> _tickObjects = new List<ITickObject>();
+	public static void AddTickObject(ITickObject tickObject)
+	{
+		if(tickObject == null)
+		{
+			Log.Warning("Warning: Don't add the null object to tick object list");
+			return;
+		}
+		if(_instance._tickObjects.Exists(t => t == tickObject))
+		{
+			Log.Warning("Warning: Don't add the same tick object twice.");
+			return;
+		}
+		_instance._tickObjects.Add(tickObject);
+	}
+
+	public static void RemoveTickObject(ITickObject tickObject)
+	{
+		if(tickObject == null)
+		{
+			Log.Warning("Warning: Can not remove the null object from the tick object list");
+			return;
+		}
+		if(!_instance._tickObjects.Remove(tickObject))
+		{
+			Log.Warning("Warning: Remove tick object error. May be the tick object is not in list.");
+		}
+	}
+	#endregion
+
 	// Use this for initialization
 	void Start () 
 	{
--- Assets/Scripts/Managers/EventManager.cs
+++ Assets/Scripts/Managers/EventManager.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using PureMVC.Interfaces;
+using Core.Interface;
 
 namespace Core.Manager
 {
@@ -36,4 +37,137 @@ namespace Core.Manager
 		}
 	}
 
+	class EventReference
+	{
+		private WeakReference _weakReference;
+		private Delegate _listener;
+		public Delegate listener
+		{
+			get
+			{
+				if(_listener != null)
+				{
+					return _listener;
+				}
+				else if(_weakReference != null)
+				{
+					return _weakReference.Target as Delegate;
+				}
+				return null;
+			}
+		}
+		private bool _isOnce;
+		public bool isOnce { get { return _isOnce; }}
+		public EventReference(Delegate listener, bool useWeakReference, bool isOnce)
+		{
+			if(useWeakReference)
+			{
+				_weakReference = new WeakReference(listener);
+			}
+			else
+			{
+				_listener = listener;
+			}
+			_isOnce = isOnce;
+		}
+	}
+
+	public class EventManager:ITickObject
+	{
+		public float weakReferenceGCDelay = 30f;
+		private float _weakReferenceGCTime = 0.0f;
+		public bool LimitQueueProcesing = false;
+		public float QueueProcessTime = 0.0f;	
+		private Hashtable _listenerTable = new Hashtable();
+		private Queue _eventQueue = new Queue();
+		private object lockObjcet = new object();
+
+		private static EventManager _instance = null;
+		public static EventManager instance
+		{
+			get
+			{
+				if(_instance == null)
+				{
+					_instance = new EventManager();
+				}
+
+				return _instance;
+			}
+		}
+
+		public void Init()
+		{
+			Engine.AddTickObject(this);
+		}
+
+		private List<EventReference> _GetEventListenerList(object context, string eventName, bool createIfNotExist = false)
+		{
+			if(!_listenerTable.ContainsKey(context))
+			{
+				if(!createIfNotExist){ return null; }
+				_listenerTable.Add(context, new Dictionary<string, List<EventReference>>());
+			}
+			IDictionary<string, List<EventReference>> dictionary = _listenerTable[context] as IDictionary<string, List<EventReference>>;
+			if(!dictionary.ContainsKey(eventName))
+			{
+				if(!createIfNotExist){ return null; }
+				dictionary[eventName] = new List<EventReference>();
+			}
+			return dictionary[eventName];
+		}
+
+		public static bool HasListener(object context, string eventName, Delegate listener)
+		{
+			return instance.HasEventListener(context, eventName, listener);
+		}
+
+		public bool HasEventListener(object context, string eventName, Delegate listener)
+		{
+			if(!_listenerTable.ContainsKey(context)) 
+				return false;
+
+			List<EventReference> list = _GetEventListenerList(context, eventName);
+			if(list == null)
+			{
+				return false;
+			}
+			if(!list.Exists(l => l.listener == listener))
+			{
+				return false;
+			}
+			return true;
+		}
+
+		public static bool AddListener<T>(object context, string eventName, T Listener, bool useWeakReference = false, bool isOnce = false)
+		{
+		}
+
+		public static 
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
